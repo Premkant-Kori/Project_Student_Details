c4eed52e7c30f08f55740736d822a960ffe5f301
Premkant Kori <101425848+Premkant-Kori@users.noreply.github.com>
2023-03-06 03:55:25 +0530

    till delete item

--- studentinfo/enroll/views.py
+++ studentinfo/enroll/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from .forms import UserForm
+from .models import User
 
-# Create your views here.
+# This function will add Item and Show all items.
+def add_show(request):
+    if request.method == 'POST':
+        fm = UserForm(request.POST)
+        if fm.is_valid():
+            nm = fm.cleaned_data['name']
+            em = fm.cleaned_data['email']
+            pw = fm.cleaned_data['password']
+            reg = User(name=nm, email=em, password=pw)
+            reg.save()
+            fm = UserForm()
+    else:
+        # will simply generate form without post data
+        fm = UserForm()    
+        # for fetching data
+    stud = User.objects.all().values()
+    return render(request, 'enroll/addandshow.html', {'form':fm, 'stu':stud})
+
+# this function will edit data
+def update_data(request, id):
+    return render(request, "enroll/updatestudent.html", {'id':id})
+
+# this function will delete item
+def delete_data(request, id):
+    if request.method == 'POST':
+        pi = User.objects.get(pk=id)
+        pi.delete()
+        return HttpResponseRedirect('/')
